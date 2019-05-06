3c48f9193bfb6e3fb3e4d0167bb773cbe4fef362
Jeff Martin <jeff@reportmill.com>
2019-05-06 16:28:20 -0500

    ScriptEditor: Moved ScriptView inside here

--- src/comics/app/ScriptEditor.java
+++ src/comics/app/ScriptEditor.java
@@ -1,5 +1,6 @@
 package comics.app;
 import comics.player.*;
+import java.util.*;
 import snap.gfx.*;
 import snap.view.*;
 
@@ -24,19 +25,18 @@ public class ScriptEditor extends ViewOwner {
     int                  _scriptLineCount;
     
     // Constants
+    static Font SCRIPTVIEW_FONT = new Font("Arial", 15);
+    static Color LINEVIEW_FILL = new Color(.9d);
     static Color INPUTTEXT_SEL_COLOR = new Color("#CDECF6");
     static String _stars[] = { "Setting", "Camera", "Lady", "Man", "Car", "Cat", "Dog", "Trump", "Obama", "Duke" };
     
 /**
  * Creates a ScriptEditor.
  */
-public ScriptEditor(EditorPane anEP)
-{
-    _editorPane = anEP;
-}
+public ScriptEditor(EditorPane anEP)  { _editorPane = anEP; }
 
 /**
- * Returns the PlayerView.
+ * Returns the Player.
  */
 public PlayerView getPlayer()  { return _editorPane.getPlayer(); }
 
@@ -45,20 +45,15 @@ public PlayerView getPlayer()  { return _editorPane.getPlayer(); }
  */
 public Script getScript()  { return _editorPane.getScript(); }
 
-/**
- * Adds a ScriptLine for given string at given index.
- */
-public void addLineText(String aStr, int anIndex)  { _editorPane.addLineText(aStr, anIndex); }
-
-/**
- * Sets ScriptLine text to given string at given index.
- */
-public void setLineText(String aStr, int anIndex)  { _editorPane.setLineText(aStr, anIndex); }
-
-/**
- * Deletes the current selected line.
- */
-public void delete()  { _editorPane.delete(); }
+/** Conveniences. */
+int getSelIndex()  { return _editorPane.getSelIndex(); }
+void setSelIndex(int anIndex)  { _editorPane.setSelIndex(anIndex); }
+ScriptLine getSelLine()  { return _editorPane.getSelLine(); }
+void selectPrev()  { _editorPane.selectPrev(); }
+void selectNext()  { _editorPane.selectNext(); }
+void selectNextWithInsert()  { _editorPane.selectNextWithInsert(); }
+void runCurrentLine()  { _editorPane.runCurrentLine(); }
+void delete()  { _editorPane.delete(); }
 
 /**
  * Sets selected ScriptLine to text for given string at given index.
@@ -76,7 +71,7 @@ public void setLineText(String aStr)
            selectNextWithInsert(); return; }
            
         // Set line to new text
-        setLineText(str, ind);
+        _editorPane.setLineText(str, ind);
     }
     
     // If new line
@@ -88,46 +83,11 @@ public void setLineText(String aStr)
             
         // Add new line
         ind = EditorPane.negateIndex(ind);
-        addLineText(str, ind);
+        _editorPane.addLineText(str, ind);
     }
     runCurrentLine();
     _scriptView.requestFocus();
 }
-
-/**
- * Returns the selected ScriptLine index.
- */
-public int getSelIndex()  { return _editorPane.getSelIndex(); }
-
-/**
- * Sets the selected ScriptLine index.
- */
-public void setSelIndex(int anIndex)  { _editorPane.setSelIndex(anIndex); } //if(anIndex>=0) runCurrentLine();
-
-/**
- * Returns the selected ScriptLine.
- */
-public ScriptLine getSelLine()  { return _editorPane.getSelLine(); }
-
-/**
- * Selects previous line.
- */
-public void selectPrev()  { _editorPane.selectPrev(); }
-
-/**
- * Selects next line.
- */
-public void selectNext()  { _editorPane.selectNext(); }
-
-/**
- * Selects next line.
- */
-public void selectNextWithInsert()  { _editorPane.selectNextWithInsert(); }
-
-/**
- * Runs the current line.
- */
-public void runCurrentLine()  { _editorPane.runCurrentLine(); }
 
 /**
  * Called when Script text changes.
@@ -139,10 +99,45 @@ protected void scriptChanged()
 }
 
 /**
- * Called when InputText does KeyRelease to catch delete.
+ * Called when ScriptView gets KeyPress event.
+ */
+void scriptViewDidKeyPress(ViewEvent anEvent)
+{
+    // Handle Delete, BackSpaceKey, Up/Down arrow
+    if(anEvent.isDeleteKey() || anEvent.isBackSpaceKey()) delete();
+    else if(anEvent.isUpArrow()) selectPrev();
+    else if(anEvent.isDownArrow()) selectNext();
+        
+    // Handle tab key
+    else if(anEvent.isTabKey()) {
+        if(anEvent.isShiftDown()) selectPrev();
+        else selectNext();
+    }
+        
+    // Handle enter key
+    else if(anEvent.isEnterKey()) {
+        if(anEvent.isShiftDown()) selectPrev();
+        else selectNextWithInsert();
+    }
+    
+    // Handle letter or digit
+    else {
+        char c = anEvent.getKeyChar();
+        if(Character.isLetterOrDigit(c)) {
+            _inputText.requestFocus();
+            _inputText.selectAll();
+            ViewUtils.processEvent(_inputText, anEvent);
+        }
+    }
+    anEvent.consume();
+}
+
+/**
+ * Called when InputText does KeyPress to handle tab & delete keys.
  */
 void inputTextDidKeyPress(ViewEvent anEvent)
 {
+    // Handle Tab key
     if(anEvent.isTabKey() && getScript().getLineCount()>0) {
         int ind = getSelIndex(); if(ind<0) ind = EditorPane.negateIndex(ind);
         ind = (ind+1) % getScript().getLineCount();
@@ -150,12 +145,29 @@ void inputTextDidKeyPress(ViewEvent anEvent)
         runCurrentLine();
     }
     
+    // Handle Delete, BackSpace keys
     if((anEvent.isDeleteKey() || anEvent.isBackSpaceKey()) && _inputText.length()==0) {
-        //ViewUtils.fireActionEvent(_inputText, anEvent);
         _inputText.escape(anEvent);
         delete();
         anEvent.consume();
     }
+}
+
+/**
+ * Called when HelpListView does Action event.
+ */
+void helpListViewDidAction(ViewEvent anEvent)
+{
+    ScriptLine line = getSelLine();
+    String starName = line!=null && line.getStar()!=null? line.getStar().getStarName() : null;
+    String str = _helpListView.getSelItem();
+    String str2 = starName!=null? starName + ' ' + str : str;
+    setLineText(str2);
+    
+    // Reset UI
+    _helpListView.setSelIndex(-1);
+    _scriptView.requestFocus();
+    resetLater();
 }
 
 /**
@@ -170,7 +182,8 @@ protected View createUI()
     RowView mainRowView = (RowView)mainColView.getChild("MainRowView"); mainRowView.setFillHeight(true);
     
     // Create/configure ScriptView add to MainRowView
-    _scriptView = new ScriptView(this);
+    _scriptView = new ScriptView();
+    _scriptView.addEventFilter(e -> scriptViewDidKeyPress(e), KeyPress);
     ScrollView scriptScrollView = new ScrollView(_scriptView);
     scriptScrollView.setGrowWidth(true); scriptScrollView.setGrowHeight(true); scriptScrollView.setPrefHeight(180);
     mainRowView.addChild(scriptScrollView, 0);
@@ -255,18 +268,8 @@ protected void respondUI(ViewEvent anEvent)
     }
     
     // Handle HelpListView
-    if(anEvent.equals("HelpListView")) {
-        _scriptView.requestFocus();
-        ScriptLine line = getSelLine();
-        String starName = line!=null && line.getStar()!=null? line.getStar().getStarName() : null;
-        String str = _helpListView.getSelItem();
-        String str2 = starName!=null? starName + ' ' + str : str;
-        ViewUtils.runOnMouseUp(() -> {
-            setLineText(str2);
-            _helpListView.setSelIndex(-1);
-            resetLater();
-        });
-    }
+    if(anEvent.equals("HelpListView"))
+        ViewUtils.runOnMouseUp(() -> helpListViewDidAction(anEvent));
     
     // Handle InputText
     if(anEvent.equals("InputText"))
@@ -279,6 +282,155 @@ protected void respondUI(ViewEvent anEvent)
     // Handle EditLineButton
     if(anEvent.equals("EditLineButton"))
         _editorPane.showLineEditor();
+}
+
+/**
+ * A view subclass to show script lines.
+ */
+protected class ScriptView extends ColView {
+    
+    // The selected index
+    int                _selIndex = -1;
+    
+    // The current list of LineViews
+    List <LineView>    _lineViews = new ArrayList();
+    
+    // The cursor lineview
+    LineView           _cursorLineView;
+    
+    // Constants
+    Color SELECT_COLOR = Color.get("#039ed3");
+    Effect SELECT_EFFECT = new ShadowEffect(8, Color.DARKGRAY, 0, 0);
+    Effect SELECT_EFFECT_FOC = new ShadowEffect(8, SELECT_COLOR, 0, 0);
+
+    /** Creates ScriptView. */
+    public ScriptView()
+    {
+        setPadding(8,8,8,12); setSpacing(6); setGrowWidth(true); setGrowHeight(true);
+        setFill(Color.WHITE); setFont(SCRIPTVIEW_FONT);
+        setFocusable(true); setFocusWhenPressed(true); setFocusKeysEnabled(false);
+        enableEvents(MousePress, Action);
+        
+        // Configure CursorLineView
+        _cursorLineView = new LineView(null); _cursorLineView.setPrefSize(150,1);
+        _cursorLineView.setMargin(0,0,0,5);
+        _cursorLineView.setFill(Color.LIGHTGRAY); _cursorLineView.setEffect(SELECT_EFFECT_FOC);
+    }
+    
+    /** Called when Script changes. */
+    protected void scriptChanged()
+    {
+        // Set script
+        Script script = ScriptEditor.this.getScript();
+        
+        // Iterate over lines and add LineView for each
+        removeChildren(); _lineViews.clear();
+        for(ScriptLine sline : script.getLines()) {
+            LineView lview = new LineView(sline);
+            addChild(lview); _lineViews.add(lview);
+        }
+        addChild(_cursorLineView);
+        
+        // Reset SelIndex
+        _selIndex = -1;
+    }
+    
+    /** Returns the selected index. */
+    public int getSelIndex()  { return _selIndex; }
+    
+    /** Sets the selected index. */
+    public void setSelIndex(int anIndex)
+    {
+        // If already set or too high, just return
+        if(anIndex==_selIndex || anIndex>=getChildCount()) return;
+        
+        // Undecorate last selected LineView
+        LineView oldSelLV = getSelLineView(); if(oldSelLV!=null) oldSelLV.setEffect(null);
+        
+        // Set new value
+        _selIndex = anIndex;
+        
+        // Decorate new selected LineView
+        LineView selLineView = getSelLineView();
+        if(selLineView!=null) { 
+            selLineView.setEffect(getSelEffect());
+            ViewUtils.runLater(() -> scrollToVisible(selLineView.getBoundsParent().getInsetRect(-5)));
+        }
+        
+        // Add/remove CursorLineView
+        if(_selIndex<0) {
+            int ind0 = indexOfChild(_cursorLineView), ind1 = EditorPane.negateIndex(_selIndex);
+            if(ind1<ind0) addChild(_cursorLineView, ind1);
+            else if(ind1>ind0) { removeChild(_cursorLineView); addChild(_cursorLineView, ind1); }
+            ViewUtils.runLater(() -> scrollToVisible(_cursorLineView.getBoundsParent().getInsetRect(-5)));
+        }
+        _cursorLineView.setVisible(_selIndex<0);
+        repaint();
+    }
+    
+    /** Returns the selected index. */
+    LineView getSelLineView()  { return _selIndex>=0? _lineViews.get(_selIndex) : null; }
+    
+    /** Handle Events. */
+    protected void processEvent(ViewEvent anEvent)
+    {
+        // Handle MousePress: Select negative index to trigger insert mode
+        if(anEvent.isMousePress()) {
+            int ind = 0; for(LineView lview : _lineViews) { if(lview.getY()+5>anEvent.getY()) break; ind++; }
+            ind = EditorPane.negateIndex(ind);
+            setSelIndex(ind);
+            fireActionEvent(anEvent); anEvent.consume();
+        }
+    }
+    
+    /** Returns the Select effect. */
+    Effect getSelEffect()  { return isFocused()? SELECT_EFFECT_FOC : SELECT_EFFECT; }
+    
+    /** Override to reset Select effect. */
+    protected void setFocused(boolean aValue)
+    {
+        if(aValue==isFocused()) return; super.setFocused(aValue);
+        for(View child : _lineViews) if(child.getEffect()!=null) child.setEffect(getSelEffect());
+    }
+}
+
+/**
+ * A class to hold a script line.
+ */
+private class LineView extends Label {
+    
+    /** Create LineView. */
+    public LineView(ScriptLine aLine)
+    {
+        setPadding(5,10,5,10); setRadius(10);
+        if(aLine!=null) { setText(aLine.getText()); setFont(SCRIPTVIEW_FONT); }
+        setFill(LINEVIEW_FILL);
+        enableEvents(MousePress);
+    }
+
+    /** Handle Events. */
+    protected void processEvent(ViewEvent anEvent)
+    {
+        // Handle MousePress
+        if(anEvent.isMousePress()) {
+            
+            // Focus ScriptView
+            _scriptView.requestFocus();
+            
+            // Handle single-click
+            if(anEvent.getClickCount()==1) {
+                int ind = _scriptView._lineViews.indexOf(this);
+                _scriptView.setSelIndex(ind);
+                ViewUtils.fireActionEvent(_scriptView, anEvent);
+            }
+            
+            // Handle multi-click
+            else _editorPane.showLineEditor();
+            
+            // Consume event
+            anEvent.consume();
+        }
+    }
 }
 
 /**
