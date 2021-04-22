74001ff143723d2d2a169ffaa23164168a1279a9
Jeff Martin <jeff@reportmill.com>
2021-04-22 14:51:12 -0500

    PropObject: Moved more support over from View and ChartPart

--- src/comics/player/Script.java
+++ src/comics/player/Script.java
@@ -5,7 +5,7 @@ import snap.util.*;
 /**
  * A class to represent the instructions.
  */
-public class Script implements PropChange.DoChange {
+public class Script extends PropObject {
     
     // The PlayerView
     PlayerView          _player;
@@ -22,235 +22,218 @@ public class Script implements PropChange.DoChange {
     // Undo/Redo texts
     Undoer              _undoer = new Undoer();
     
-    // PropertyChangeSupport
-    PropChangeSupport   _pcs = PropChangeSupport.EMPTY;
-
     // Constants
     public static final String Line_Prop = "Line";
     public static final String Text_Prop = "Text";
 
-/**
- * Creates a script for given PlayerView.
- */
-public Script(PlayerView aPlayer)  { _player = aPlayer; }
+    /**
+     * Creates a script for given PlayerView.
+     */
+    public Script(PlayerView aPlayer)  { _player = aPlayer; }
 
-/**
- * Returns the Player.
- */
-public PlayerView getPlayer()  { return _player; }
+    /**
+     * Returns the Player.
+     */
+    public PlayerView getPlayer()  { return _player; }
 
-/**
- * Returns the Stage.
- */
-public StageView getStage()  { return _player.getStage(); }
+    /**
+     * Returns the Stage.
+     */
+    public StageView getStage()  { return _player.getStage(); }
 
-/**
- * Returns the Script text.
- */
-public String getText()  { return _text; }
+    /**
+     * Returns the Script text.
+     */
+    public String getText()  { return _text; }
 
-/**
- * Sets the text.
- */
-public void setText(String aStr)
-{
-    // Set text, reset Lines, RunTime, notify player
-    _text = aStr; _lines = null; _runTime = -1;
-    _player.scriptChanged();
-}
-
-/**
- * Returns the number of lines.
- */
-public int getLineCount()  { return getLines().size(); }
-
-/**
- * Returns the line at given index.
- */
-public ScriptLine getLine(int anIndex)  { return getLines().get(anIndex); }
-
-/**
- * Returns the lines.
- */
-public List <ScriptLine> getLines()
-{
-    // If already set, just return
-    if(_lines!=null) return _lines;
-    
-    // Get text lines from text
-    List slines = new ArrayList();
-    String tlines[] = _text.split("\\n");
-    
-    // Iterate over text lines and create ScriptLines
-    for(String tline : tlines)
-        slines.add(new ScriptLine(this, tline));
-    return _lines = slines;
-}
-
-/**
- * Adds a line to script.
- */
-public void addLine(ScriptLine aLine, int anIndex)
-{
-    getLines().add(anIndex, aLine);
-    firePropChange(Line_Prop, null, aLine, anIndex);
-    _runTime = -1; _player.scriptChanged();
-}
-
-/**
- * Removes a line.
- */
-public ScriptLine removeLine(int anIndex)
-{
-    ScriptLine sline = getLines().remove(anIndex);
-    firePropChange(Line_Prop, sline, null, anIndex);
-    _runTime = -1; _player.scriptChanged();
-    return sline;
-}
-
-/**
- * Adds a line to script.
- */
-public void addLineText(String aStr, int anIndex)
-{
-    ScriptLine sline = new ScriptLine(this, aStr);
-    addLine(sline, anIndex);
-}
-
-/**
- * Sets a line in script to new string.
- */
-public void setLineText(String aStr, int anIndex)
-{
-    ScriptLine sline = getLine(anIndex);
-    sline.setText(aStr);
-}
-
-/**
- * Returns the run time.
- */
-public int getRunTime()
-{
-    if(_runTime>=0) return _runTime;
-    _runTime = 0; for(ScriptLine line : getLines()) _runTime += line.getRunTime();
-    return _runTime;
-}
-
-/**
- * Returns the run time of line at index.
- */
-public int getLineRunTime(int anIndex)  { return anIndex<getLineCount()? getLine(anIndex).getRunTime() : 0; }
-
-/**
- * Returns the runtime for line.
- */
-public int getLineStartTime(int aLine)  { int rt = 0; for(int i=0;i<aLine;i++) rt += getLineRunTime(i); return rt; }
-
-/**
- * Returns the runtime for line.
- */
-public int getLineEndTime(int aLine)  { int rt = 0; for(int i=0;i<=aLine;i++) rt += getLineRunTime(i); return rt; }
-
-/**
- * Returns the run line for given run time.
- */
-public int getLineForTime(int aTime)
-{
-    int time = aTime, lineCount = getLineCount();
-    for(int i=0;i<lineCount;i++) { time -= getLineRunTime(i); if(time<0) return i; }
-    return lineCount - 1;
-}
-
-/**
- * Runs the script.
- */
-public void runLine(int anIndex)
-{
-    // If invalid line index, just return
-    if(anIndex>=getLineCount()) { System.err.println("Script.runLine: Index beyond bounds."); return; }
-
-    // Run requested line
-    ScriptLine line = getLine(anIndex);
-    line.run();
-}
-
-/**
- * Returns the Undoer.
- */
-public Undoer getUndoer()  { return _undoer; }
-
-/**
- * Undo text.
- */
-public void undo()  { _undoer.undo(); }
-
-/**
- * Redo text.
- */
-public void redo()  { _undoer.redo(); }
-
-/**
- * Called when a ScriptLine changes.
- */
-void scriptLineDidChange(PropChange aPC)
-{
-    _undoer.addPropChange(aPC);
-    _undoer.saveChanges();
-    _runTime = -1; _player.scriptChanged();
-}
-
-/**
- * Returns the given feet size in points.
- */
-public double feetToPoints(double aValue)
-{
-    double stageHeightPoints = _player.getStage().getHeight(), stageHeightFeet = 11.64;
-    double pointHeight = aValue*stageHeightPoints/stageHeightFeet;
-    pointHeight = Math.round(pointHeight);
-    return pointHeight;
-}
-
-/**
- * Add listener.
- */
-public void addPropChangeListener(PropChangeListener aPCL)
-{
-    if(_pcs==PropChangeSupport.EMPTY) _pcs = new PropChangeSupport(this);
-    _pcs.addPropChangeListener(aPCL);
-}
-
-/**
- * Remove listener.
- */
-public void removePropChangeListener(PropChangeListener aPCL)  { _pcs.removePropChangeListener(aPCL); }
-
-/**
- * Fires a property change for given property name, old value, new value and index.
- */
-protected void firePropChange(String aProp, Object oldVal, Object newVal, int anIndex)
-{
-    PropChange pc = new PropChange(this, aProp, oldVal, newVal, anIndex);
-    _pcs.firePropChange(pc);
-    _undoer.addPropChange(pc);
-    _undoer.saveChanges();
-}
-
-/**
- * PropChange.DoChange method.
- */
-public void doChange(PropChange aPC, Object oldVal, Object newVal)
-{
-    String prop = aPC.getPropName();
-    if(prop==Line_Prop) { int ind = aPC.getIndex();
-        if(oldVal==null) {
-            addLine((ScriptLine)newVal, ind);
-            getPlayer().playLine(ind);
-        }
-        else {
-            removeLine(ind);
-            if(ind>0) getPlayer().playLine(ind-1);
-        }
+    /**
+     * Sets the text.
+     */
+    public void setText(String aStr)
+    {
+        // Set text, reset Lines, RunTime, notify player
+        _text = aStr; _lines = null; _runTime = -1;
+        _player.scriptChanged();
     }
-}
 
+    /**
+     * Returns the number of lines.
+     */
+    public int getLineCount()  { return getLines().size(); }
+
+    /**
+     * Returns the line at given index.
+     */
+    public ScriptLine getLine(int anIndex)  { return getLines().get(anIndex); }
+
+    /**
+     * Returns the lines.
+     */
+    public List <ScriptLine> getLines()
+    {
+        // If already set, just return
+        if(_lines!=null) return _lines;
+
+        // Get text lines from text
+        List slines = new ArrayList();
+        String tlines[] = _text.split("\\n");
+
+        // Iterate over text lines and create ScriptLines
+        for(String tline : tlines)
+            slines.add(new ScriptLine(this, tline));
+        return _lines = slines;
+    }
+
+    /**
+     * Adds a line to script.
+     */
+    public void addLine(ScriptLine aLine, int anIndex)
+    {
+        getLines().add(anIndex, aLine);
+        firePropChange(Line_Prop, null, aLine, anIndex);
+        _runTime = -1; _player.scriptChanged();
+    }
+
+    /**
+     * Removes a line.
+     */
+    public ScriptLine removeLine(int anIndex)
+    {
+        ScriptLine sline = getLines().remove(anIndex);
+        firePropChange(Line_Prop, sline, null, anIndex);
+        _runTime = -1; _player.scriptChanged();
+        return sline;
+    }
+
+    /**
+     * Adds a line to script.
+     */
+    public void addLineText(String aStr, int anIndex)
+    {
+        ScriptLine sline = new ScriptLine(this, aStr);
+        addLine(sline, anIndex);
+    }
+
+    /**
+     * Sets a line in script to new string.
+     */
+    public void setLineText(String aStr, int anIndex)
+    {
+        ScriptLine sline = getLine(anIndex);
+        sline.setText(aStr);
+    }
+
+    /**
+     * Returns the run time.
+     */
+    public int getRunTime()
+    {
+        if(_runTime>=0) return _runTime;
+        _runTime = 0; for(ScriptLine line : getLines()) _runTime += line.getRunTime();
+        return _runTime;
+    }
+
+    /**
+     * Returns the run time of line at index.
+     */
+    public int getLineRunTime(int anIndex)  { return anIndex<getLineCount()? getLine(anIndex).getRunTime() : 0; }
+
+    /**
+     * Returns the runtime for line.
+     */
+    public int getLineStartTime(int aLine)  { int rt = 0; for(int i=0;i<aLine;i++) rt += getLineRunTime(i); return rt; }
+
+    /**
+     * Returns the runtime for line.
+     */
+    public int getLineEndTime(int aLine)  { int rt = 0; for(int i=0;i<=aLine;i++) rt += getLineRunTime(i); return rt; }
+
+    /**
+     * Returns the run line for given run time.
+     */
+    public int getLineForTime(int aTime)
+    {
+        int time = aTime, lineCount = getLineCount();
+        for(int i=0;i<lineCount;i++) { time -= getLineRunTime(i); if(time<0) return i; }
+        return lineCount - 1;
+    }
+
+    /**
+     * Runs the script.
+     */
+    public void runLine(int anIndex)
+    {
+        // If invalid line index, just return
+        if(anIndex>=getLineCount()) { System.err.println("Script.runLine: Index beyond bounds."); return; }
+
+        // Run requested line
+        ScriptLine line = getLine(anIndex);
+        line.run();
+    }
+
+    /**
+     * Returns the Undoer.
+     */
+    public Undoer getUndoer()  { return _undoer; }
+
+    /**
+     * Undo text.
+     */
+    public void undo()  { _undoer.undo(); }
+
+    /**
+     * Redo text.
+     */
+    public void redo()  { _undoer.redo(); }
+
+    /**
+     * Called when a ScriptLine changes.
+     */
+    void scriptLineDidChange(PropChange aPC)
+    {
+        _undoer.addPropChange(aPC);
+        _undoer.saveChanges();
+        _runTime = -1; _player.scriptChanged();
+    }
+
+    /**
+     * Returns the given feet size in points.
+     */
+    public double feetToPoints(double aValue)
+    {
+        double stageHeightPoints = _player.getStage().getHeight(), stageHeightFeet = 11.64;
+        double pointHeight = aValue*stageHeightPoints/stageHeightFeet;
+        pointHeight = Math.round(pointHeight);
+        return pointHeight;
+    }
+
+    /**
+     * Fires a property change for given property name, old value, new value and index.
+     */
+    protected void firePropChange(PropChange aPC)
+    {
+        super.firePropChange(aPC);
+        _undoer.addPropChange(aPC);
+        _undoer.saveChanges();
+    }
+
+    /**
+     * PropChange.DoChange method.
+     */
+    @Override
+    public void processPropChange(PropChange aPC, Object oldVal, Object newVal)
+    {
+        String prop = aPC.getPropName();
+        if(prop==Line_Prop) { int ind = aPC.getIndex();
+            if(oldVal==null) {
+                addLine((ScriptLine)newVal, ind);
+                getPlayer().playLine(ind);
+            }
+            else {
+                removeLine(ind);
+                if(ind>0) getPlayer().playLine(ind-1);
+            }
+        }
+        else super.processPropChange(aPC, oldVal, newVal);
+    }
 }
--- src/comics/player/ScriptLine.java
+++ src/comics/player/ScriptLine.java
@@ -5,7 +5,7 @@ import snap.util.*;
 /**
  * A class to represent a line of script.
  */
-public class ScriptLine implements PropChange.DoChange, Loadable {
+public class ScriptLine extends PropObject implements Loadable {
     
     // The Script
     Script             _script;
@@ -22,230 +22,204 @@ public class ScriptLine implements PropChange.DoChange, Loadable {
     // The action
     Action             _action;
     
-    // PropertyChangeSupport
-    PropChangeSupport  _pcs = PropChangeSupport.EMPTY;
-
     // Constants
     public static final String Text_Prop = "Text";
     
-/**
- * Creates a new ScriptLine with given text.
- */
-public ScriptLine(Script aScript, String aStr)
-{
-    _script = aScript; _text = aStr;
-    addPropChangeListener(pc -> _script.scriptLineDidChange(pc));
-}
-
-/**
- * Returns the script.
- */
-public Script getScript()  { return _script; }
-
-/**
- * Returns the text.
- */
-public String getText()  { return _text; }
-
-/**
- * Sets the text.
- */
-public void setText(String aStr)
-{
-    _words = null; _star = null; _action = null;
-    firePropChange(Text_Prop, _text, _text = aStr);
-}
-
-/**
- * Returns the words.
- */
-public String[] getWords()
-{
-    // If already set, just return
-    if(_words!=null) return _words;
-    
-    // Get text and words
-    String text = getText().toLowerCase().replace(","," ").replace("\"","");
-    String words[] = text.split("\\s");
-    return _words = words;
-}
-
-/**
- * Returns the index.
- */
-public int getIndex()  { return _script.getLines().indexOf(this); }
-
-/**
- * Returns the star name.
- */
-public String getStarName()
-{
-    String words[] = getWords(); if(words.length==0) return null;
-    String word = words[0];
-    return word.length()>0? word : null;
-}
-
-/**
- * Returns the action name.
- */
-public String getActionName()
-{
-    String words[] = getWords(); if(words.length<2) return null;
-    String word = words[1];
-    return word.length()>0? word : null;
-}
-
-/**
- * Returns the Star of this line.
- */
-public Star getStar()
-{
-    if(_star!=null) return _star;
-    return getStarImpl();
-}
-
-/**
- * Returns the Star of this line.
- */
-protected Star getStarImpl()
-{
-    String name = getStarName(); if(name==null) return null;
-    if(name.equals("setting")) return _script.getStage();
-    if(name.equals("camera")) return _script._player.getCamera();
-    Star star = _script.getStage().getActor(this);
-    if(star==null) System.out.println("ScriptLine: Couldn't find star named: " + name);
-    return star;
-}
-
-/**
- * Sets the Star.
- */
-public void setStar(Star aStar)
-{
-    String words[] = getWords();
-    String text = aStar.getStarName();
-    for(int i=1;i<words.length;i++) text += ' ' + words[i];
-    getScript().setLineText(text, getIndex());
-}
-
-/**
- * Returns the action.
- */
-public Action getAction()
-{
-    // If already set, just return
-    if(_action!=null) return _action;
-    
-    // Get Star and action
-    Star star = getStar(); if(star==null) return null;
-    return _action = star.getAction(this);
-}
-
-/**
- * Sets the action by name.
- */
-public void setActionByName(String aName)
-{
-    Star star = getStar(); if(star==null) return;
-    String text = star.getStarName() + ' ' + aName;
-    getScript().setLineText(text, getIndex());
-}
-
-/**
- * Returns the runtime of this ScriptLine.
- */
-public int getRunTime()  { Action a = getAction(); return a!=null? a.getRunTime() : 1; }
-
-/**
- * Executes line.
- */
-public void run()
-{
-    // Get star and make sure it's visible
-    Star star = getStar(); if(star==null) return;
-    if(star instanceof Actor && !((Actor)star).isVisible()) { Actor actr = (Actor)star;
-        actr.setVisible(true); actr.setLocXY(Pos.BOTTOM_LEFT, 10, 10, null); }
-
-    // Get action and run        
-    Action action = getAction(); if(action==null) return;
-    action.run();
-}
-
-/**
- * Returns whether resource is loaded.
- */
-public boolean isLoaded()
-{
-    Loadable loadable = getLoadable();
-    return loadable==null || loadable.isLoaded();
-}
-
-/**
- * Adds a callback to be triggered when resources loaded (cleared automatically when loaded).
- */
-public void addLoadListener(Runnable aRun)
-{
-    Loadable loadable = getLoadable(); if(loadable==null) return;
-    loadable.addLoadListener(aRun);
-}
-
-/**
- * Returns the loadable.
- */
-protected Loadable getLoadable()
-{
-    if(_loadable!=null) return _loadable;
-    Star star = getStar();
-    Action action = getAction();
-    if(star!=null && !star.isLoaded()) System.out.println("ScriptLine.getLoadable: Star not loaded");
-    if(action!=null && !action.isLoaded()) System.out.println("ScriptLine.getLoadable: Action not loaded");
-    _loadable = Loadable.getAsLoadable(star, action);
-    return _loadable;
-}
-Loadable _loadable;
-    
-/**
- * Standard toString implementation.
- */
-public String toString()
-{
-    String str = getText(); if(str.trim().length()==0) str = "(empty)";
-    return "SriptLine: " + str;
-}
-
-/**
- * Add listener.
- */
-public void addPropChangeListener(PropChangeListener aPCL)
-{
-    if(_pcs==PropChangeSupport.EMPTY) _pcs = new PropChangeSupport(this);
-    _pcs.addPropChangeListener(aPCL);
-}
-
-/**
- * Remove listener.
- */
-public void removePropChangeListener(PropChangeListener aPCL)  { _pcs.removePropChangeListener(aPCL); }
-
-/**
- * Fires a property change for given property name, old value, new value and index.
- */
-protected void firePropChange(String aProp, Object oldVal, Object newVal)
-{
-    if(!_pcs.hasListener(aProp)) return;
-    _pcs.firePropChange(new PropChange(this, aProp, oldVal, newVal));
-}
-
-/**
- * PropChange.DoChange method.
- */
-public void doChange(PropChange aPC, Object oldVal, Object newVal)
-{
-    String prop = aPC.getPropName();
-    if(prop==Text_Prop) {
-        setText(SnapUtils.stringValue(newVal));
-        getScript().getPlayer().playLine(getIndex());
+    /**
+     * Creates a new ScriptLine with given text.
+     */
+    public ScriptLine(Script aScript, String aStr)
+    {
+        _script = aScript; _text = aStr;
+        addPropChangeListener(pc -> _script.scriptLineDidChange(pc));
     }
-}
 
+    /**
+     * Returns the script.
+     */
+    public Script getScript()  { return _script; }
+
+    /**
+     * Returns the text.
+     */
+    public String getText()  { return _text; }
+
+    /**
+     * Sets the text.
+     */
+    public void setText(String aStr)
+    {
+        _words = null; _star = null; _action = null;
+        firePropChange(Text_Prop, _text, _text = aStr);
+    }
+
+    /**
+     * Returns the words.
+     */
+    public String[] getWords()
+    {
+        // If already set, just return
+        if(_words!=null) return _words;
+
+        // Get text and words
+        String text = getText().toLowerCase().replace(","," ").replace("\"","");
+        String words[] = text.split("\\s");
+        return _words = words;
+    }
+
+    /**
+     * Returns the index.
+     */
+    public int getIndex()  { return _script.getLines().indexOf(this); }
+
+    /**
+     * Returns the star name.
+     */
+    public String getStarName()
+    {
+        String words[] = getWords(); if(words.length==0) return null;
+        String word = words[0];
+        return word.length()>0? word : null;
+    }
+
+    /**
+     * Returns the action name.
+     */
+    public String getActionName()
+    {
+        String words[] = getWords(); if(words.length<2) return null;
+        String word = words[1];
+        return word.length()>0? word : null;
+    }
+
+    /**
+     * Returns the Star of this line.
+     */
+    public Star getStar()
+    {
+        if(_star!=null) return _star;
+        return getStarImpl();
+    }
+
+    /**
+     * Returns the Star of this line.
+     */
+    protected Star getStarImpl()
+    {
+        String name = getStarName(); if(name==null) return null;
+        if(name.equals("setting")) return _script.getStage();
+        if(name.equals("camera")) return _script._player.getCamera();
+        Star star = _script.getStage().getActor(this);
+        if(star==null) System.out.println("ScriptLine: Couldn't find star named: " + name);
+        return star;
+    }
+
+    /**
+     * Sets the Star.
+     */
+    public void setStar(Star aStar)
+    {
+        String words[] = getWords();
+        String text = aStar.getStarName();
+        for(int i=1;i<words.length;i++) text += ' ' + words[i];
+        getScript().setLineText(text, getIndex());
+    }
+
+    /**
+     * Returns the action.
+     */
+    public Action getAction()
+    {
+        // If already set, just return
+        if(_action!=null) return _action;
+
+        // Get Star and action
+        Star star = getStar(); if(star==null) return null;
+        return _action = star.getAction(this);
+    }
+
+    /**
+     * Sets the action by name.
+     */
+    public void setActionByName(String aName)
+    {
+        Star star = getStar(); if(star==null) return;
+        String text = star.getStarName() + ' ' + aName;
+        getScript().setLineText(text, getIndex());
+    }
+
+    /**
+     * Returns the runtime of this ScriptLine.
+     */
+    public int getRunTime()  { Action a = getAction(); return a!=null? a.getRunTime() : 1; }
+
+    /**
+     * Executes line.
+     */
+    public void run()
+    {
+        // Get star and make sure it's visible
+        Star star = getStar(); if(star==null) return;
+        if(star instanceof Actor && !((Actor)star).isVisible()) { Actor actr = (Actor)star;
+            actr.setVisible(true); actr.setLocXY(Pos.BOTTOM_LEFT, 10, 10, null); }
+
+        // Get action and run
+        Action action = getAction(); if(action==null) return;
+        action.run();
+    }
+
+    /**
+     * Returns whether resource is loaded.
+     */
+    public boolean isLoaded()
+    {
+        Loadable loadable = getLoadable();
+        return loadable==null || loadable.isLoaded();
+    }
+
+    /**
+     * Adds a callback to be triggered when resources loaded (cleared automatically when loaded).
+     */
+    public void addLoadListener(Runnable aRun)
+    {
+        Loadable loadable = getLoadable(); if(loadable==null) return;
+        loadable.addLoadListener(aRun);
+    }
+
+    /**
+     * Returns the loadable.
+     */
+    protected Loadable getLoadable()
+    {
+        if(_loadable!=null) return _loadable;
+        Star star = getStar();
+        Action action = getAction();
+        if(star!=null && !star.isLoaded()) System.out.println("ScriptLine.getLoadable: Star not loaded");
+        if(action!=null && !action.isLoaded()) System.out.println("ScriptLine.getLoadable: Action not loaded");
+        _loadable = Loadable.getAsLoadable(star, action);
+        return _loadable;
+    }
+    Loadable _loadable;
+
+    /**
+     * Standard toString implementation.
+     */
+    public String toString()
+    {
+        String str = getText(); if(str.trim().length()==0) str = "(empty)";
+        return "SriptLine: " + str;
+    }
+
+    /**
+     * PropChange.DoChange method.
+     */
+    public void processPropChange(PropChange aPC, Object oldVal, Object newVal)
+    {
+        String prop = aPC.getPropName();
+        if(prop==Text_Prop) {
+            setText(SnapUtils.stringValue(newVal));
+            getScript().getPlayer().playLine(getIndex());
+        }
+        else super.processPropChange(aPC, oldVal, newVal);
+    }
 }
